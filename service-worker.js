657f25f11ed506011d3f05a6c8f15ca4de43c9b8
franklin <franklin.garoma@gmail.com>
2026-01-30 16:21:43 -0300

    subindo pm

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "kids-video-cache-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./css/style.css",
+        "./js/app.js",
+        "./js/videos.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
